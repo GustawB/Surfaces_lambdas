69048766245f29708f13081071a8725889bcb13e
GustawB <gustaw.blachowski@gmail.com>
2024-01-23 11:11:54 +0100

    Implement plain and Surface functors.

--- CPP_7/surfaces.h
+++ CPP_7/surfaces.h
@@ -8,9 +8,6 @@ using std::ostream;
 
 class Point
 {
-private:
-	Real point_x;
-	Real point_y;
 public:
 	Point() = delete;
 	Point(const Real&, const Real&); // Only constructor;
@@ -18,19 +15,62 @@ public:
 	~Point() = default;
 
 	friend ostream& operator<<(ostream&, const Point&);
+
+	Real x;
+	Real y;
 };
 
 inline Point::Point(const Real& a, const Real& b)
 {
-	point_x = a;
-	point_y = b;
+	x = a;
+	y = b;
 }
 
 inline ostream& operator<<(ostream& os, const Point& p)
 {
-	os << p.point_x << " " << p.point_y;
+	os << p.x << " " << p.y;
 	return os;
 }
+
+class Surface
+{
+public:
+	Surface();
+	Surface(const Real&, const Real&);
+
+protected:
+	Real point_x;
+	Real point_y;
+};
+
+inline Surface::Surface()
+{
+	point_x = 0.0;
+	point_y = 0.0;
+}
+
+inline Surface::Surface(const Real& x, const Real& y)
+{
+	point_x = x;
+	point_y = y;
+}
+
+class plain : public Surface
+{
+public:
+	plain();
+	plain(const Real&, const Real&);
+	Real operator()(const Point& x)
+	{
+		return 0.0;
+	}
+};
+
+inline plain::plain() : Surface()
+{}
+
+inline plain::plain(const Real& x, const Real& y) : Surface(x, y)
+{}
 
 #endif
 
